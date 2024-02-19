98c8b53513a4ca70998839211a1ce540319270ed
Prathap Dhanasekaran <prathapprof@gmail.com>
2024-02-19 20:29:15 +0100

    Git Authentication done and tested!

--- src/main/java/com/example/sociallogin/Controller/Controller.java
+++ src/main/java/com/example/sociallogin/Controller/Controller.java
@@ -1,4 +1,4 @@
-package com.example.sociallogin;
+package com.example.sociallogin.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
