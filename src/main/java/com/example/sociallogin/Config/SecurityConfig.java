1b93ab347c9534bd2eec36c19371e236c15d7ef7
Prathap Dhanasekaran <prathapprof@gmail.com>
2024-02-19 19:58:59 +0100

    Initial commit

---
+++ src/main/java/com/example/sociallogin/Config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.example.sociallogin.Config;public class SecurityConfig {
+}
---
+++ src/main/java/com/example/sociallogin/Controller/Controller.java
@@ -0,0 +1,21 @@
+package com.example.sociallogin;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping ("/")
+    public String home()
+    {
+        return "hello Home";
+    }
+
+    @GetMapping ("/secured")
+    public String secure()
+    {
+        return "hello Secured";
+    }
+
+}
